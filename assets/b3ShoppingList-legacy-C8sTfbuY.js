f594e5ff67ee605568fa4c2c01feae09517af69f
Sergio Piedrabuena <piedrabuena.sergio.f@gmail.com>
2024-10-15 12:19:02 -0300

    update

---
+++ assets/b3ShoppingList-legacy-C8sTfbuY.js
@@ -0,0 +1,1 @@
+System.register(["./react-setup-legacy-Cfb8SlNa.js","../index-legacy.CVtY65ui.js"],(function(e,s){"use strict";var t,n,a,c;return{setters:[e=>{t=e.aA,n=e.a6,a=e.aZ,c=e.a_},null],execute:function(){e("c",(({data:e,isB2BUser:s})=>{const i=e,r=s?a:c;if(s){const{submitShoppingListPermission:e}=t();i.status=e?30:0}else i.channelId=n;return r({...i,channelId:n})}))}}}));
