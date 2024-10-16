3ea4be20f50bbe1d4e438b2f280ef50f5ffcc737
Sergio Piedrabuena <piedrabuena.sergio.f@gmail.com>
2024-10-16 12:12:46 -0300

    fix(login): label inputs estilos

---
+++ assets/useBlockPendingAccountViewPrice-BsoEUyQS.js
@@ -0,0 +1,1 @@
+import{u as e}from"./react-setup-BZAh8p9K.js";const i=()=>[e(({global:c})=>c.blockPendingAccountViewPrice)];export{i as u};
---
+++ history/v1_0_5/assets/pdf-legacy-y9y3S6xo.js
@@ -0,0 +1,1 @@
+System.register(["./reactVendor-legacy-CtYIj4Ma.js"],(function(e,t){"use strict";return{setters:[null],execute:function(){}}}));
