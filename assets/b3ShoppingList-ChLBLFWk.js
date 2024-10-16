cd4c7c98e27367a13627615bc535dcaed59a9066
Sergio Piedrabuena <piedrabuena.sergio.f@gmail.com>
2024-10-16 11:08:56 -0300

    feat(login): login rediseño

---
+++ assets/b3ShoppingList-ChLBLFWk.js
@@ -0,0 +1,1 @@
+import{aA as o,a6 as a,aZ as c,a_ as p}from"./react-setup-C-R_NROw.js";import"../index.KQ48E-g-.js";const g=({data:e,isB2BUser:s})=>{const t=e,i=s?c:p;if(s){const{submitShoppingListPermission:n}=o();t.status=n?30:0}else t.channelId=a;return i({...t,channelId:a})};export{g as c};
---
+++ history/v1_0_3/assets/index-legacy-Dg_kaycb.js
@@ -0,0 +1,1 @@
+System.register([],(function(t,e){"use strict";return{execute:function(){t("default",(function(){return null}))}}}));
