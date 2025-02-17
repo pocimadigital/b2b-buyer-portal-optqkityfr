c62f9cbe5ae8ebb3db8979d184070e7b1107028c
Sergio Piedrabuena <piedrabuena.sergio.f@gmail.com>
2025-02-17 12:41:08 -0300

    FEAT(global): cambio de colores 1b5f90 por 232A59

---
+++ assets/b3ShoppingList-Bl6DIWWo.js
@@ -0,0 +1,1 @@
+import{aA as o,a6 as a,aZ as c,a_ as p}from"./react-setup-DQCmcDDd.js";import"../index.s9J4ziE2.js";const g=({data:e,isB2BUser:s})=>{const t=e,i=s?c:p;if(s){const{submitShoppingListPermission:n}=o();t.status=n?30:0}else t.channelId=a;return i({...t,channelId:a})};export{g as c};
