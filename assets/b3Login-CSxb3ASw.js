c26890967bccdd3928aeae0755155e79592c46d4
Sergio Piedrabuena <piedrabuena.sergio.f@gmail.com>
2025-02-18 10:11:23 -0300

    FEAT(global): cambio de colores EA617C por 4E3F92

---
+++ assets/b3Login-CSxb3ASw.js
@@ -0,0 +1,1 @@
+import{ag as a}from"./react-setup-0pxRNfGw.js";const i=(e,n=!1)=>{const{global:{loginLandingLocation:s,recordOpenHash:o,setOpenPageFn:r}}=a.getState();if(s==="1"&&!o)return e("/"),r==null||r({isOpen:!1,openUrl:""}),n&&window.sessionStorage.clear(),window.location.reload(),!1;if(s==="1"&&o){const t=o.split("#")[1];return e(t),!1}return!0};export{i as l};
---
+++ assets/useBlockPendingAccountViewPrice-CkW985Gz.js
@@ -0,0 +1,1 @@
+import{u as e}from"./react-setup-0pxRNfGw.js";const i=()=>[e(({global:c})=>c.blockPendingAccountViewPrice)];export{i as u};
