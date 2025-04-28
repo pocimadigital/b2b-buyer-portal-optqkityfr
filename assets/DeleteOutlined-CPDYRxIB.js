722b9c0695cb3696bf798995152895a806b9138b
Sergio Piedrabuena <piedrabuena.sergio.f@gmail.com>
2025-04-28 12:46:07 -0300

    FIX(global): remove console log

---
+++ assets/DeleteOutlined-CPDYRxIB.js
@@ -0,0 +1,1 @@
+import{o as t,j as r}from"./mui-CyG7s_nf.js";import{r as a}from"./react-setup-CrWNYFBB.js";var e={},u=t;Object.defineProperty(e,"__esModule",{value:!0});var o=e.default=void 0,i=u(a()),l=r;o=e.default=(0,i.default)((0,l.jsx)("path",{d:"M16 9v10H8V9zm-1.5-6h-5l-1 1H5v2h14V4h-3.5zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2z"}),"DeleteOutlined");export{o as d};
